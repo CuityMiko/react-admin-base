e33c835c23bd49a92a998d9023a53c43100a619d
崔同洋 <cuitongyang@chuangjiangx.com>
2018-11-15 14:00:45 +0800

    修改文件

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import './style/index.less';
 const middleware = [thunk];
 const store = createStore(reducer, applyMiddleware(...middleware));
 
-// 增加react-hot-loader保持状态刷新操作，如果不需要可去掉并把下面注释的打开
+// 增加react-hot-loader保持状态刷新操作
 const render = Component => {
     ReactDOM.render(
         <AppContainer>
@@ -32,9 +32,6 @@ render(Page);
 
 // Webpack Hot Module Replacement API
 if (module.hot) {
-    // 隐藏You cannot change <Router routes>; it will be ignored 错误提示
-    // react-hot-loader 使用在react-router 3.x上引起的提示，react-router 4.x不存在
-    // 详情可参照https://github.com/gaearon/react-hot-loader/issues/298
     const orgError = console.error; // eslint-disable-line no-console
     console.error = (...args) => { // eslint-disable-line no-console
         if (args && args.length === 1 && typeof args[0] === 'string' && args[0].indexOf('You cannot change <Router routes>;') > -1) {
@@ -49,17 +46,7 @@ if (module.hot) {
     })
 }
 
-// ReactDOM.render(
-//     <AppContainer>
-//         <Provider store={store}>
-//             <Page store={store} />
-//         </Provider>
-//     </AppContainer>
-//  ,
-//   document.getElementById('root')
-// );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
